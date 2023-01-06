38fb766ad09d5f50308a4450868ceb077e8863d1
Guillermo <guillef33@gmail.com>
2023-01-05 23:12:23 -0300

    blog post cards and page

--- archive.php
+++ archive.php
@@ -7,29 +7,37 @@ get_header();
     <div class="container">
 
         <div class="row">
-            <div class="col-lg-3">
+            <!-- <div class="col-lg-3">
                 <div class="sticky-top" style="top:50px">
                     <?php get_sidebar(); ?>
                 </div>
-            </div>
-            <div class="col-lg-9">
+            </div> -->
+            <div class="col-lg-12">
                 <div class="blog-post">
-                    <?php if (has_post_thumbnail()) : ?>
-                        <img src="<?php the_post_thumbnail_url(''); ?>" />
-                    <?php endif; ?>
-
                     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
-                            <a href="<?php the_permalink(); ?>">
-                                <h1><?php the_title(); ?></h1>
-                            </a>
-                            <?php the_excerpt(); ?>
+                            <div class="card">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php
+                                    if (has_post_thumbnail()) : ?>
+                                        <img src="<?php the_post_thumbnail_url(''); ?>" class="card-img-top" />
+                                    <?php endif; ?>
+                                </a>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php the_title(); ?></h5>
+                                    <p class="card-text"><?php the_excerpt(); ?></p>
+                                    <p class="card-text">By: <?php the_author(); ?></p>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <button class="btn btn-dark">Read More</button>
+                                    </a>
+                                </div>
+                            </div>
 
                     <?php endwhile;
                     else : endif; ?>
                 </div>
             </div>
+
+
 
         </div>
     </div>
