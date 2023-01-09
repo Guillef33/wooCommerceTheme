1006fef8f2b984ff514f9b9bbcbbbb610b746d9e
Guillermo <guillef33@gmail.com>
2023-01-09 10:34:38 -0300

    header.php

--- header.php
+++ header.php
@@ -8,9 +8,17 @@
  * @package Lucky Store
 
  */
-wp_head();
-
 ?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <?php wp_head(); ?>
+</head>
 
 <body <?php body_class('test'); ?> <header>
     <nav class="navbar navbar-expand-lg " style="background-color: white;">
