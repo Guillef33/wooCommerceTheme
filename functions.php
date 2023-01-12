c36dbfbe761904c6954a2ad61d3aad6adcf3a446
Guillermo <guillef33@gmail.com>
2023-01-12 11:23:42 -0300

    funciones de woo pasan a woo-custom

--- functions.php
+++ functions.php
@@ -207,7 +207,7 @@ function customJavascript()
                 },
             });
         </script>
-    <?php
+<?php
     }
 }
 add_action('wp_head', 'customJavascript');
@@ -329,40 +329,12 @@ add_action('customize_register', 'frontPageEditable');
 
 add_action('woocommerce_single_product_summary', 'show_subtitle');
 
+// function my_excerpt_length($length)
+// {
+//     return 10;
+// }
+// add_filter('excerpt_length', 'my_excerpt_length');
 
-
-function show_subtitle()
-{
-    global $product;
-
-    $id = $product->get_id();
-
-    $category =    $product->get_category_ids();
-
-    $array = implode(" ", $category);
-
-    ?>
-    <form>
-        <h5>Calcula las cuotas de acuerdo a tu tarjeta</h5>
-        <select>
-            <option name="american" style="background-image: url (" <?php bloginfo('template_directory'); ?>/images/logosBancos/amex.jpg");">
-                <img src="<?php bloginfo('template_directory'); ?>/images/logosBancos/amex.jpg" class="d-block w-100" alt="...">
-                AMEX
-            </option>
-            <option name="visa">VISA</option>
-            <option name="master">Master Card</option>
-            <option name="master">Uala</option>
-        </select>
-        <select>
-            <option name="una">1</option>
-            <option name="tres">3</option>
-            <option name="seis">6</option>
-
-        </select>
-        <button type="submit">Calcular interes</button>
-        <p>El precio final es $precio</p>
-    </form>
-<?php
+if (class_exists('WooCommerce')) {
+    require get_template_directory() . '/inc/woo-custom.php';
 }
-
-add_action('woocommerce_after_shop_loop_item_title', 'the_excerpt', 1);
---
+++ inc/woo-custom.php
@@ -0,0 +1,41 @@
+
+<?php
+
+function show_subtitle()
+{
+    global $product;
+
+    $id = $product->get_id();
+
+    $category =    $product->get_category_ids();
+
+    $array = implode(" ", $category);
+
+    ?>
+    <form>
+        <h5>Calcula las cuotas de acuerdo a tu tarjeta</h5>
+        <select>
+            <option name="american" style="background-image: url (" <?php bloginfo('template_directory'); ?>/images/logosBancos/amex.jpg");">
+                <img src="<?php bloginfo('template_directory'); ?>/images/logosBancos/amex.jpg" class="d-block w-100" alt="...">
+                AMEX
+            </option>
+            <option name="visa">VISA</option>
+            <option name="master">Master Card</option>
+            <option name="master">Uala</option>
+        </select>
+        <select>
+            <option name="una">1</option>
+            <option name="tres">3</option>
+            <option name="seis">6</option>
+
+        </select>
+        <button type="submit">Calcular interes</button>
+        <p>El precio final es $precio</p>
+    </form>
+<?php
+}
+
+add_action('woocommerce_after_shop_loop_item_title', 'the_excerpt', 1);
+
+
+?>
