53aeefc50abaeea6ffdb9b27e33a357ad5dc7514
Guillermo <guillef33@gmail.com>
2023-01-12 11:38:42 -0300

    grid search, estilos woo en woo, transition en cards

--- search.php
+++ search.php
@@ -40,40 +40,47 @@ $type = 'product';
     // }
     $query = new WP_Query($args);
     ?>
-    <h4> Searching for: <?php echo $text; ?></h4>
-    <?php
-    if ($query->have_posts()) { ?>
+    <div class="search-breadcrumb">
+        <h4> Searching for: <?php echo $text; ?></h4>
+    </div>
+    <div class="card-grid-search-results">
         <?php
+        if ($query->have_posts()) { ?>
+            <?php
 
-        while ($query->have_posts()) : $query->the_post();
-        ?>
+            while ($query->have_posts()) : $query->the_post();
+            ?>
 
 
-            <div class="card">
-                <a href="<?php the_permalink(); ?>">
-                    <?php
-                    if (has_post_thumbnail()) : ?>
-                        <img src="<?php the_post_thumbnail_url(''); ?>" class="card-img-top" />
-                    <?php endif; ?>
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title"><?php the_title(); ?></h5>
-                    <p class="card-text"><?php the_excerpt(); ?></p>
+                <div class="card">
                     <a href="<?php the_permalink(); ?>">
-                        <button class="btn btn-dark">Ver producto</button>
+                        <?php
+                        if (has_post_thumbnail()) : ?>
+                            <img src="<?php the_post_thumbnail_url(''); ?>" class="card-img-top" />
+                        <?php endif; ?>
                     </a>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title(); ?></h5>
+                        <p class="card-text"><?php the_excerpt(); ?></p>
+                        <a href="<?php the_permalink(); ?>">
+                            <button class="btn btn-dark">Ver producto</button>
+                        </a>
+                    </div>
                 </div>
-            </div>
 
 
-    <?php endwhile;
-    } else {
-        echo '<p>No post found</p>';
-    }
-    //  wp_reset_query(); 
-    wp_reset_postdata();
 
-    ?>
+            <?php endwhile;
+            ?>
+    </div>
+<?php
+        } else {
+            echo '<p>No post found</p>';
+        }
+        //  wp_reset_query(); 
+        wp_reset_postdata();
+
+?>
 
 </div>
 
